8918d5f857bb6674e8523aed0d869d22a9d7fecf
MariaNihad <155116591+MariaNihad@users.noreply.github.com>
2024-01-03 21:04:31 +0100

    Update Liste.h

--- algoprjt/Liste.h
+++ algoprjt/Liste.h
@@ -6,28 +6,28 @@ struct Noeud {
     struct Noeud* suivant;
 };
 
-// Fonction pour créer un nouveau nœud
+// Fonction pour crÃ©er un nouveau nÅ“ud
 struct Noeud* creerNoeud(int valeur);
 
-// Fonction pour insérer un élément au début de la liste
+// Fonction pour insÃ©rer un Ã©lÃ©ment au dÃ©but de la liste
 void insererAuDebut(struct Noeud** tete, int valeur);
 
-// Fonction pour insérer un élément à la fin de la liste
+// Fonction pour insÃ©rer un Ã©lÃ©ment Ã  la fin de la liste
 void insererALaFin(struct Noeud** tete, int valeur);
 
-// Fonction pour supprimer un élément au début de la liste
+// Fonction pour supprimer un Ã©lÃ©ment au dÃ©but de la liste
 void supprimerAuDebut(struct Noeud** tete);
 
-// Fonction pour supprimer un élément à la fin de la liste
+// Fonction pour supprimer un Ã©lÃ©ment Ã  la fin de la liste
 void supprimerALaFin(struct Noeud** tete);
 
-// Fonction pour rechercher un élément dans la liste
+// Fonction pour rechercher un Ã©lÃ©ment dans la liste
 struct Noeud* rechercherElement(struct Noeud* tete, int valeur);
 
 // Fonction pour trier la liste par insertion
 void trierParInsertion(struct Noeud** tete);
 
-// Fonction pour afficher les éléments de la liste
+// Fonction pour afficher les Ã©lÃ©ments de la liste
 void afficherListe(struct Noeud* tete);
 
 #endif // !INCLUDED_H_LISTE
